e5906f1ba4a07975c9bbe0d1007bb1885c43ddb7
Dan Pashin <danypashin@yandex.ru>
2026-04-13 10:21:18 +0300

    Update to the 2026 branch

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -exo pipefail
 
-export VAULT_VERSION="v2025.5.0"
+#export VAULT_VERSION="v2026.1.1"
+export VAULT_VERSION="v2026.1.1+0"
 
 VAULT_GIT_VERSION="main"
 VAULT_SERVER_VERSION="$VAULT_GIT_VERSION"
